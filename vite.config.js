c774a8b4f6d36ce4f11d40354fcb806e94546b67
ritesmoond <moondritesh41@gmail.com>
2026-04-21 11:46:39 +0530

    push

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/fezap/',   // 🔥 YE ADD KARNA HAI
+  plugins: [react(), tailwindcss()],
   server: {
     historyApiFallback: true
   }
