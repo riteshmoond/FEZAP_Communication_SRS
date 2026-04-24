e9232308c83466489c5bef57e8f8f28a670d8045
ritesmoond <moondritesh41@gmail.com>
2026-04-24 11:52:01 +0530

    base uptade

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/fezap/',   // 🔥 YE ADD KARNA HAI
+  base: '/fezap_frontend/',   // 🔥 YE ADD KARNA HAI
   plugins: [react(), tailwindcss()],
   server: {
     historyApiFallback: true,
